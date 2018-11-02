ba8092b6fc60d6346dda498092dadfbae7eb465d
Alex Angas <alexangas@gmail.com>
2018-11-02 15:33:25 +0000

    Added release comparison benchmark (#31)

---
+++ tests/FastWildcard.Performance.NuGet/Benchmarks/Configs/CompareVersionsConfig.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace FastWildcard.Performance.NuGet.Benchmarks.Configs
+{
+    public class CompareVersionsConfig : ManualConfig
+    {
+        public CompareVersionsConfig()
+        {
+            const string nugetPackageName = "FastWildcard";
+            string[] nugetPackageVersions = {"2.0.1", "3.0.0", "3.1.0"};
+            var ltsRuntime = CsProjCoreToolchain.NetCoreApp21;
+            var currentRuntime = CsProjCoreToolchain.NetCoreApp22;
+
+            foreach (var version in nugetPackageVersions)
+            foreach (var toolchain in new[] {ltsRuntime, currentRuntime})
+            {
+                Add(Job.MediumRun
+                    .With(toolchain)
+                    .WithNuGet(nugetPackageName, version));
+            }
+        }
+    }
+}
---
+++ tests/FastWildcard.Performance/Benchmarks/Configs/CompareRuntimesConfig.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.CsProj;
+
+namespace FastWildcard.Performance.Benchmarks.Configs
+{
+    public class CompareRuntimesConfig : ManualConfig
+    {
+        public CompareRuntimesConfig()
+        {
+            Add(Job.Default.With(CsProjCoreToolchain.NetCoreApp22));
+            Add(Job.Default.With(CsProjCoreToolchain.NetCoreApp21));
+            Add(Job.Default.With(CsProjCoreToolchain.NetCoreApp20));
+            Add(Job.Default.With(CsProjClassicNetToolchain.Net472));
+            Add(Job.Default.With(CsProjClassicNetToolchain.Net461));
+        }
+    }
+}
